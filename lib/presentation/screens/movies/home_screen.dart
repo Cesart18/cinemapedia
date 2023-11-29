f9f11d47c0d155cc5310017bef13d2366bf7e1f1
Cesart18 <cjataonline@gmail.com>
2023-11-28 23:34:05 -0400

    seccion 12 finalizada

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,5 +1,8 @@
+import 'package:cinemapedia/presentation/providers/movies/movies_providers.dart';
+import 'package:cinemapedia/presentation/providers/movies/movies_repository_provider.dart';
 import 'package:cinemapedia/presentation/widgets/side_menu/side_menu.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -12,15 +15,44 @@ class HomeScreen extends StatelessWidget {
       key: scaffoldKey,
       // appBar: AppBar(),
 
-    // body
+      // body
 
-    body:  Placeholder(),
+      body: const _HomeVeiw(),
 
-    // drawer
+      // drawer
 
       drawer: SideMenu(
         scaffoldKey: scaffoldKey,
       ),
     );
+  }
+}
+
+class _HomeVeiw extends ConsumerStatefulWidget {
+  const _HomeVeiw();
+
+  @override
+  _HomeVeiwState createState() => _HomeVeiwState();
+}
+
+class _HomeVeiwState extends ConsumerState<_HomeVeiw> {
+  @override
+  void initState() {
+    super.initState();
+    ref.read(nowPlayingMoviesProvider.notifier).loadNextPage();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final moviesProvider = ref.watch(nowPlayingMoviesProvider);
+    return ListView.builder(
+        itemCount: moviesProvider.length,
+        itemBuilder: (context, index) {
+          final movies = moviesProvider[index];
+          return ListTile(
+            title: Text(movies.title),
+            subtitle: Text(movies.originalTitle),
+          );
+        });
   }
 }
